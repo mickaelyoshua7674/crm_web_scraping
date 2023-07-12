c4df5ca48a69ad982c8b0d9b11844a99d883819b
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-07-12 12:09:59 -0300

    first full code to collect data

--- get_crm_data.py
+++ get_crm_data.py
@@ -2,14 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import time
-import random
+import time, random, json
 
 CHROMEDRIVER_PATH = "chromedriver.exe"
 
 def random_sleep(i: int, f: int) -> None:
     """Randomly choose a float number between i-f and sleep during that random time"""
     time.sleep(random.uniform(i, f))
+
+def get_doctor_data(div) -> dict:
+    """Get the data from the given div object"""
+    name = div.find_element(By.TAG_NAME, "h4").text
+    random_sleep(1,3)
+    crm, data_inscricao, prim_inscricao, inscricao = [t.text.split(" ")[-1] for t in div.find_elements(By.CSS_SELECTOR, ".col-md-4")]
+    random_sleep(1,3)
+    situacao = div.find_element(By.CSS_SELECTOR, ".col-md").text.split(" ")[-1]
+    random_sleep(1,3)
+    endereco = div.find_element(By.CSS_SELECTOR, ".row.endereco").find_element(By.TAG_NAME, "div").text
+    random_sleep(1,3)
+    telefone = div.find_element(By.CSS_SELECTOR, ".row.telefone").find_element(By.TAG_NAME, "div").text
+    return {
+        "crm": crm,
+        "name": name,
+        "data_inscricao": data_inscricao,
+        "prim_inscricao": prim_inscricao,
+        "inscricao": inscricao,
+        "situacao": situacao,
+        "endereco": endereco,
+        "telefone": telefone
+    }
 
 # op = webdriver.ChromeOptions()
 # op.add_argument("headless") # don't open a Chrome window
@@ -28,27 +49,45 @@ random_sleep(1,3)
 driver.find_element(By.TAG_NAME, "body").click()
 random_sleep(1,3)
 driver.find_element(By.CSS_SELECTOR, ".w-100.btn-buscar.btnPesquisar").click()
+print("Page 1")
 random_sleep(5,7)
 driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
 random_sleep(1,3)
 
+# GET FIRST PAGE DATA AND SAVE
+print("Getting data...")
+divs_med = [d for d in driver.find_element(By.CSS_SELECTOR, ".busca-resultado").find_elements(By.TAG_NAME, "div")
+            if "resultado-item resultMedico_" in d.get_attribute("class")]
+random_sleep(1,3)
+data = [get_doctor_data(div) for div in divs_med]
+print("Data collected.\n")
+with open("crm_data.json", "w") as f:
+    json.dump(data, f)
+
+# GET DATA FROM THE FOLLOWING PAGES
 last_page = int([p.get_attribute("data-num") for p in driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")][-1])
 print(last_page)
-random_sleep(1,3)
-for p in range(1, last_page+1):
-    pages_list = driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")
+for p in range(2, last_page+1):
+    random_sleep(5,7)
+    [li for li in driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")
+     if li.get_attribute("data-num") == str(p)][0].click()
+    print(f"Page {p}")
+    random_sleep(7,9)
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.HOME)
+    
+    print("Getting data...")
+    divs_med = [d for d in driver.find_element(By.CSS_SELECTOR, ".busca-resultado").find_elements(By.TAG_NAME, "div")
+                if "resultado-item resultMedico_" in d.get_attribute("class")]
     random_sleep(1,3)
-    for obj in pages_list:
-        if obj.get_attribute("data-num") == str(p):
-            obj.click()
-            print(f"In page {p}")
-            random_sleep(5,7)
-            driver.find_element(By.TAG_NAME, "body").send_keys(Keys.HOME)
-            random_sleep(1,3)
-            driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
-            random_sleep(1,3)
+    data = [get_doctor_data(div) for div in divs_med]
+    print("Data collected.\n")
+    
+    with open("crm_data.json", "r") as f:
+        data += json.load(f)
+    with open("crm_data.json", "w") as f:
+        json.dump(data, f)
 
-
-
-
+    random_sleep(1,3)
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
+    random_sleep(1,3)
 driver.quit()
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import random
-print(random.uniform(1, 5))
+fullstring = "StackAbuse"
+substring = "tack"
+
+if substring in fullstring:
+    print("Found!")
+else:
+    print("Not found!")
