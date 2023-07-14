318bc3bb4a6b81fd30fc83cfdac7e2ebdd3b496c
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-07-14 12:26:55 -0300

    bla

--- get_crm_data.py
+++ get_crm_data.py
@@ -2,7 +2,6 @@ from helper import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from typing import List
 import pandas as pd
 from os.path import exists
 from threading import Thread
