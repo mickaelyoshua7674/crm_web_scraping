4508f4859840811dcd0b6b6db33e91ec568a4984
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-08-14 09:47:13 -0300

    implement count loop for keep in the same page

--- get_crm_data.py
+++ get_crm_data.py
@@ -1,20 +1,19 @@
 from helper import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from os.path import exists
 import pandas as pd
 import pickle as pk
 
 CHROMEDRIVER_PATH = "chromedriver.exe"
-COLUMNS = ["crm", "name", "data_inscricao", "prim_inscricao", "inscricao", "situacao", "endereco", "telefone"]
+COLUMNS = ["crm", "nome", "data_inscricao", "prim_inscricao", "inscricao", "situacao", "endereco", "telefone"]
 
 if not exists("crm_pb_data.csv"): # if file don't exist create an empty csv
     pd.DataFrame(columns=COLUMNS).to_csv("crm_pb_data.csv", header=True, index=False)
 
 # init last collected page as 0, if there is a variable saved then load the variable
 last_collected_page = 0
-if exists("last_collected_page.pk"):
+if exists("last_collected_page.pkl"):
     with open("last_collected_page.pkl", "rb") as f:
         last_collected_page = pk.load(f)
 
@@ -28,37 +27,26 @@ fill_form(driver)
 # get last page
 last_page = int([p.get_attribute("data-num") for p in driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")][-1])
 if last_collected_page > 0:
-    pages = [p for p in range(last_collected_page+1, last_page+1)]
-    go_to_last_collected_page(driver, last_collected_page)
-else:
-    pages = [p for p in range(1, last_page+1)]
+    go_to_last_collected_page(driver, last_collected_page+1)
 
-for p in pages:
-    if p == 1:
-        # GET FIRST PAGE DATA AND SAVE
-        random_sleep(1,3)
-        print_current_hour()
-        print("Getting data...")
-        data = get_doctors_data(driver)
-        print("Data collected.\n")
-        pd.DataFrame(data=data, columns=COLUMNS).to_csv("crm_pb_data.csv", header=False, index=False, mode="a") # concatenate into dataframe
-    else:
-        print_current_hour()
-        random_sleep(5,7)
-        click_page(driver, p)
-        print(f"Page {p}")
-        random_sleep(7,9)
-        driver.find_element(By.TAG_NAME, "body").send_keys(Keys.HOME) # Click Home button and go up
-        
+active_page = get_active_page(driver)
+current_page = 0
+loop_count = 0
+while active_page < last_page or loop_count < 10:
+    print_current_hour()
+    print(f"Page {active_page}")
+    
+    if active_page != current_page: # after clicking to next page, if it is now a different page, then collect data
         print("Getting data...")
         data = get_doctors_data(driver)
         print("Data collected.\n")
         pd.DataFrame(data=data, columns=COLUMNS).to_csv("crm_pb_data.csv", header=False, index=False, mode="a")
-        random_sleep(1,3)
-
-        driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END) # Click End button and go down
-        random_sleep(1,3)
-        
+        current_page = active_page
         with open("last_collected_page.pkl", "wb") as f:
-            pk.dump(p, f) # save last page collected
+            pk.dump(active_page, f) # save last page collected
+    else:
+        loop_count += 1
+
+    click_page(driver, active_page+1)
+    active_page = get_active_page(driver)
 driver.quit()
--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from typing import List
 import time, random
 from datetime import datetime
 
@@ -17,25 +18,24 @@ def fill_form(driver) -> None:
     """Fill UF as PB and show search"""
     # SELECT UF -> PB / SHOW DATA
     driver.execute_script("arguments[0].scrollIntoView(true);", driver.find_element(By.CSS_SELECTOR, ".form.panel"))
-    random_sleep(1,3)
+    random_sleep(1,2)
     driver.find_element(By.ID, "uf").click()
-    random_sleep(1,3)
+    random_sleep(1,2)
     Select(driver.find_element(By.ID, "uf")).select_by_value("PB")
     print("UF PB selected.")
-    random_sleep(1,3)
+    random_sleep(1,2)
     driver.find_element(By.TAG_NAME, "body").click()
-    random_sleep(1,3)
+    random_sleep(1,2)
     driver.find_element(By.CSS_SELECTOR, ".w-100.btn-buscar.btnPesquisar").click()
     random_sleep(5,7)
     driver.find_element(By.TAG_NAME, "body").send_keys(Keys.END)
     random_sleep(1,3)
 
-def get_doctors_data(driver) -> dict:
+def get_doctors_data(driver) -> List[List[str]]:
     """Get the data from the given div object"""
     divs_med = [d for d in driver.find_element(By.CSS_SELECTOR, ".busca-resultado").find_elements(By.TAG_NAME, "div")
                     if "resultado-item resultMedico_" in d.get_attribute("class")] # get all doctor blocks
     random_sleep(1,3)
-
     return [[div.find_element(By.TAG_NAME, "h4").text,
             *[t.text.split(" ")[-1] for t in div.find_elements(By.CSS_SELECTOR, ".col-md-4")],
             div.find_element(By.CSS_SELECTOR, ".col-md").text.split(" ")[-1],
@@ -47,8 +47,10 @@ def click_page(driver, page: int) -> None:
     """Click the given page"""
     [li for li in driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")
     if li.get_attribute("data-num") == str(page)][0].click()
+    random_sleep(5,8)
 
 def go_to_last_collected_page(driver, last_collected_page: int) -> None:
+    """Go to page given page"""
     while True:
         random_sleep(3,5)
         pages_list_obj = [li for li in driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages").find_elements(By.TAG_NAME, "li")]
@@ -57,3 +59,11 @@ def go_to_last_collected_page(driver, last_collected_page: int) -> None:
             break
         except ValueError:
             pages_list_obj[-3].click()
+    random_sleep(3,5)
+
+def get_active_page(driver) -> int:
+    """Collect the current page"""
+    random_sleep(1,2)
+    return int(driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages")\
+                     .find_element(By.CSS_SELECTOR, ".paginationjs-page.J-paginationjs-page.active")\
+                     .get_attribute("data-num"))
