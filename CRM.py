89bc22d543603feca80f6a5c144ebfac615716cd
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-08-31 10:38:10 -0300

    include the Especialidade in fill form and loop on it

--- CRM.py
+++ CRM.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from typing import List, Union
 import time, random, traceback
+import pandas as pd
 
 class CRM():
     def __init__(self, chromedriver_path: str) -> None:
@@ -12,16 +13,28 @@ class CRM():
         self.driver = self.init_driver()
 
     def init_driver(self):
-        # op = webdriver.ChromeOptions()
+        op = webdriver.ChromeOptions()
+        op.add_argument("log-level=3") # https://stackoverflow.com/questions/46744968/how-to-suppress-console-error-warning-info-messages-when-executing-selenium-pyth
         # op.add_argument("headless") # don't open a Chrome window
         sc = Service(self.CHROMEDRIVER_PATH)
-        return webdriver.Chrome(service=sc)#, options=op)
+        return webdriver.Chrome(service=sc, options=op)
 
     def random_sleep(self, i: Union[int, float], f: Union[int, float]) -> None:
         """Randomly choose a float number between i-f and sleep during that random time"""
         time.sleep(random.uniform(i, f))
 
-    def _fill_field(self, id: str, value: str) -> None:
+    def get_values_from_field_form(self, id: str) -> List[str]:
+        """Get all available values in form's field"""
+        elem = self.driver.find_element(By.ID, id)
+        self.random_sleep(1,2)
+        return [e.text for e in elem.find_elements(By.TAG_NAME, "option")]
+
+    def fill_field(self, id: str, value: str) -> None:
+        """
+        Find the field element and click it
+        Select the desired value
+        Click on 'body'
+        """
         self.driver.find_element(By.ID, id).click()
         self.random_sleep(1,2)
         Select(self.driver.find_element(By.ID, id)).select_by_visible_text(value)
@@ -30,15 +43,15 @@ class CRM():
         self.driver.find_element(By.TAG_NAME, "body").click()
         self.random_sleep(1,2)
 
-    def fill_form(self, uf: str, mun: str, espec: str="Todas") -> None:
+    def fill_form(self, uf: str, mun: str="Todos", espec: str="Todas") -> None:
         """Fill UF as PB and show search"""
         # SELECT UF -> PB / SHOW DATA
         self.driver.execute_script("arguments[0].scrollIntoView(true);", self.driver.find_element(By.CSS_SELECTOR, ".form.panel"))
         self.random_sleep(1,2)
 
-        self._fill_field("uf", uf)
-        self._fill_field("municipio", mun)
-        self._fill_field("especialidade", espec)
+        self.fill_field("uf", uf)
+        self.fill_field("municipio", "João Pessoa")
+        self.fill_field("especialidade", espec)
 
         self.driver.find_element(By.CSS_SELECTOR, ".w-100.btn-buscar.btnPesquisar").click()
         self.random_sleep(5,7)
@@ -51,7 +64,7 @@ class CRM():
     def get_doctors_data(self) -> List[List[str]]:
         """Get the data from the given div object"""
         divs_med = [d for d in self.driver.find_element(By.CSS_SELECTOR, ".busca-resultado").find_elements(By.TAG_NAME, "div")] # get all doctor blocks
-        self.random_sleep(1,3)
+        print([div.find_element(By.CLASS_NAME, "card-body").text for div in divs_med])
         return [[div.find_element(By.TAG_NAME, "h4").text,
                 *[t.text.split(" ")[-1] for t in div.find_elements(By.CSS_SELECTOR, ".col-md-4")],
                 div.find_element(By.CSS_SELECTOR, ".col-md").text.split(" ")[-1],
@@ -87,3 +100,17 @@ class CRM():
         return int(self.driver.find_element(By.CSS_SELECTOR, ".paginationjs-pages")\
                         .find_element(By.CSS_SELECTOR, ".paginationjs-page.J-paginationjs-page.active")\
                         .get_attribute("data-num"))
+    
+    def concat_data(self, file_name: str, columns: List[str]):
+        print("Getting data...")
+        self.random_sleep(1,2)
+        data = self.get_doctors_data()
+        print("Data collected.\n")
+
+        collected_crm = pd.read_csv(file_name)["crm"].unique()
+        for crm in [d[1] for d in data]:
+            if crm in collected_crm:
+                print("Data already collected.")
+                exit()
+                
+        pd.DataFrame(data=data, columns=columns).to_csv(file_name, header=False, index=False, mode="a")
--- get_crm_data.py
+++ get_crm_data.py
@@ -1,54 +1,63 @@
 from CRM import CRM
-from os.path import exists
 import pandas as pd
 import pickle as pk
+import os
 
 FILE_NAME = "crm_pb_data.csv"
+LAST_PAGE_NAME = "last_collected_page.pkl"
+LAST_ESPEC_NAMNE = "last_collected_espec.pkl"
 COLUMNS = ["nome", "crm", "data_inscricao", "prim_inscricao", "inscricao", "situacao", "endereco", "telefone"]
 CHROMEDRIVER_PATH = "chromedriver.exe"
 
-if not exists(FILE_NAME): # if file don't exist create an empty csv
+if not os.path.exists(FILE_NAME): # if file don't exist create an empty csv
     pd.DataFrame(columns=COLUMNS).to_csv(FILE_NAME, header=True, index=False)
 
 # init last collected page as 0, if there is a variable saved then load the variable
 last_collected_page = 0
-if exists("last_collected_page.pkl"):
-    with open("last_collected_page.pkl", "rb") as f:
+if os.path.exists(LAST_PAGE_NAME):
+    with open(LAST_PAGE_NAME, "rb") as f:
         last_collected_page = pk.load(f)
 
+# init last collected espec as "Todas", if there is a variable saved then load the variable
+# init with "Todas" because when split the list the split will happen with the given value, so with "Todas" the split will just remove the element "Todas"
+last_collected_espec = "Todas"
+if os.path.exists(LAST_ESPEC_NAMNE):
+    with open(LAST_ESPEC_NAMNE, "rb") as f:
+        last_collected_espec = pk.load(f)
+
+# Init object
 crm_bot = CRM(CHROMEDRIVER_PATH)
 crm_bot.driver.get("https://crmpb.org.br/busca-medicos/")
-crm_bot.random_sleep(3,5)
-crm_bot.fill_form(uf="PB", mun="João Pessoa")
-crm_bot.random_sleep(8,9)
 
-# last_page = crm_bot.get_last_page()
-# if 0 < last_collected_page < last_page:
-#     print("Going to last collected page...")
-#     crm_bot.go_to_page(last_collected_page+1)
+espec = crm_bot.get_values_from_field_form("especialidade")
+crm_bot.random_sleep(1,2)
 
-# active_page = crm_bot.get_active_page()
-# current_page = loop_count = 0
-# while active_page < last_page or loop_count < 10:
-    
-#     if active_page != current_page: # after clicking to next page, if it is now a different page, then collect data
-#         print("Getting data...")
-#         crm_bot.random_sleep(1,2)
-#         data = crm_bot.get_doctors_data()
-#         print("Data collected.\n")
+for e in espec[espec.index(last_collected_espec)+1:]:
+    crm_bot.fill_form(uf="PB", espec=e)
+    crm_bot.random_sleep(5,6)
 
-#         for crm in [d[1] for d in data]:
-#             if crm in pd.read_csv(FILE_NAME)["crm"].unique():
-#                 print("Data already collected.")
-#                 exit()
-                
-#         pd.DataFrame(data=data, columns=COLUMNS).to_csv(FILE_NAME, header=False, index=False, mode="a")
-#         current_page = active_page
-#         with open("last_collected_page.pkl", "wb") as f:
-#             pk.dump(active_page, f) # save last page collected
-#     else:
-#         loop_count += 1
+    last_page = crm_bot.get_last_page()
+    if 0 < last_collected_page < last_page:
+        print("Going to last collected page...")
+        crm_bot.go_to_page(last_collected_page+1)
 
-#     crm_bot.go_to_page(active_page+1)
-#     active_page = crm_bot.get_active_page()
-# crm_bot.driver.quit()
+    active_page = crm_bot.get_active_page()
+
+    if active_page == last_page:
+        crm_bot.concat_data(FILE_NAME, COLUMNS)
+
+    else:
+        while active_page < last_page:
+            crm_bot.concat_data(FILE_NAME, COLUMNS)
+            
+            with open(LAST_PAGE_NAME, "wb") as f:
+                pk.dump(active_page, f) # save last page collected
+
+            crm_bot.go_to_page(active_page+1)
+            active_page = crm_bot.get_active_page()
+
+    with open(LAST_ESPEC_NAMNE, "wb") as f:
+        pk.dump(e, f)
+    if os.path.exists(LAST_PAGE_NAME):
+        os.remove(LAST_PAGE_NAME)
+crm_bot.driver.quit()
